e029b8231791bb9703e108cbdb2ea185a3d0c48b
jphoun96 <jphoun@butler.edu>
2016-04-25 22:12:12 -0400

    updated and running

--- JTT.java
+++ JTT.java
@@ -6,6 +6,8 @@ import java.awt.*;
 
 public class JTT extends JFrame
 {
+	private static JButton[] cells;
+	
 	class window extends WindowAdapter
 	{
 		public void Closing(WindowEvent e)
@@ -15,34 +17,6 @@ public class JTT extends JFrame
 		}
 	}
 
-	class Button
-	{
-		int x, y;
-		int size;
-		Color color;
-
-		public Button(int xpos, int ypos, int s, Color c)
-		{
-			x=xpos; y=ypos; color=c;
-			size=s;
-		}
-
-		public void colorme(Color c)
-		{
-			color=c;
-		}
-
-		public void drawme(Graphics g)
-		{
-			g.setColor(Color.blue);
-			g.fillOval(x,
-				(int)(y+0.05*size),
-				(int)(1.0*size),(int)(1.0*size));
-		}
-	}
-
-	Button [] cells;
-
 	class Picture extends JPanel
 	{
 		//set size of background
@@ -50,31 +24,30 @@ public class JTT extends JFrame
 		{
 			setSize(900,700);
 			//setLayout(new BorderLayout());
-			//setContentPane(new JLabel(new ImageIcon("back.png")));
+		    //setContentPane(new JLabel(new ImageIcon("back2.png")));
 			//setLayout(null);
 		}
 
 		//set color of background
 		public void paintComponent(Graphics g)
 		{
+
 			g.setColor(Color.white);
 			g.fillRect(0,0,900,700);
-			
-			g.setColor(Color.green);
+
+			g.setColor(Color.white);
 			g.fillRect(225,550,90,90);
-			
-			g.setColor(Color.green);
+
+			g.setColor(Color.white);
 			g.fillRect(550,550,90,90);
-			
-			
-			for(int i=0; i<9; i++)
-			{
-				cells[i].drawme(g);
-			}
+
+
 		}
+		
 	}
 
 	Picture r;
+	
 
 	public JTT()
 	{
@@ -82,45 +55,49 @@ public class JTT extends JFrame
 		setTitle("Jerry-Tac-Toe");
 		//size of window
 		setSize(900,700);
+				
+		cells = new JButton[9];
+
+		for(int i=0; i<9; i++)
+			{
+				cells[i] = new JButton("something awesome", null );
+				add(cells[i]);
+				cells[i].setBackground(Color.BLUE);
+				cells[i].setOpaque(true);	
+			}
+				
+		cells[0].setBounds(50,25,88,88);
+		cells[1].setBounds(400,25,88,88);
+		cells[2].setBounds(750,25,88,88);
+		cells[3].setBounds(200,225,88,88);
+		cells[4].setBounds(400,225,88,88);
+		cells[5].setBounds(600,225,88,88);
+		cells[6].setBounds(50,400,88,88);
+		cells[7].setBounds(400,400,88,88);
+		cells[8].setBounds(750,400,88,88);
+
 		
-		cells= new Button[9];
-		
-		cells[0]= new Button(
-		50,25,85,Color.blue);
-
-		cells[1]= new Button(
-		400,25,85,Color.blue);
-
-		cells[2]= new Button(
-		750,25,85,Color.blue);
-
-		cells[3]= new Button(
-		200,225,85,Color.blue);
-
-		cells[4]= new Button(
-		400,225,85,Color.blue);
-
-		cells[5]= new Button(
-		600,225,85,Color.blue);
-
-		cells[6]= new Button(
-		50,400,85,Color.blue);
-
-		cells[7]= new Button(
-		400,400,85,Color.blue);
-
-		cells[8]= new Button(
-		750,400,85,Color.blue);
-
-
 		Container stuff=getContentPane();
 		stuff.setLayout( new BorderLayout());
-		r=new Picture();
+		r = new Picture();
 		stuff.add(r, "Center" );
-
+		
 		setVisible(true);
+		
+		initialize();
 	}
 
+	public void initialize()
+	{
+		for (int i = 0; i < 9; i++) 
+		{
+			char ch = (char) ('0' + i + 1);
+			cells[i].setText("" + ch);
+			//cells[i].setIcon(neutralIcon);
+		}
+
+	}
+	
 	public static void main(String [] args)
 	{
 		JTT thing = new JTT();
